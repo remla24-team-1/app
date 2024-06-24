f88937ee3152c195dd07659261e9253410ce500e
flaviogheri <gheriarnaudflavio@gmail.com>
2024-06-24 11:22:35 +0200

    changed server back to in class example

--- app-service/server.py
+++ app-service/server.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 import remlaversionutilpy
 import requests
 
+from random import random
+
+
 from prometheus_client import Counter, generate_latest, start_http_server
 
 REQUEST_COUNTER = Counter('num_requests', 'Number of requests served by the application', ['endpoint'])
@@ -25,12 +28,16 @@ CORS(app)
 
 model_service_url = os.getenv('MODEL_SERVICE_URL')
 
-
+countIdx = 0
+countSub = 0
 
 
 @app.route('/')
 def index():
-    REQUEST_COUNTER.labels(endpoint='/').inc()
+    global countIdx
+    countIdx += 1
+
+    # REQUEST_COUNTER.labels(endpoint='/').inc()
     # count_index += 1
     return render_template('index.html')
 
@@ -40,9 +47,11 @@ def version():
 
 @app.route('/check-url', methods=['POST'])
 def check_url():
+    global countSub
+    countSub += 1
     # global count_predictions
     # count_predictions += 1
-    PREDICTION_COUNTER.inc()
+    # PREDICTION_COUNTER.inc()
     
     json = request.get_json()
     for i in range(10):
@@ -52,15 +61,33 @@ def check_url():
 
 @app.route('/metrics', methods=['GET'])
 def metrics():
+    global countIdx, countSub
+
+    m = "# HELP my_random This is just a random 'gauge' for illustration.\n"
+    m+= "# TYPE my_random gauge\n"
+    m+= "my_random " + str(random()) + "\n\n"
+
+    m+= "# HELP num_requests The number of requests that have been served, by page.\n"
+    m+= "# TYPE num_requests counter\n"
+    m+= "num_requests{{page=\"index\"}} {}\n".format(countIdx)
+    m+= "num_requests{{page=\"sub\"}} {}\n\n".format(countSub)
+
+    m+= "# HELP index_relevance The percentage of requests that are served by index.\n"
+    m+= "# TYPE index_relevance gauge\n"
+    m += "index_relevance {}\n".format(min(1.0, float(countIdx)) if countSub == 0 else countIdx/countSub)
+
+
+
     # global count_index
     # m = "num_requests{{page=\"index\"}} {}\n".format(count_index)
     # m += "num_predictions {}\n".format(count_predictions) 
-    return Response(generate_latest(), mimetype=str('text/plain; version=0.0.4; charset=utf-8'))
+    # return Response(generate_latest(), mimetype=str('text/plain; version=0.0.4; charset=utf-8'))
+    return Response(m, mimetype="text/plain")
 
 # @app.route('/my_metrics', methods=['POST'])
 # def post_metrics():
 #     return f'metrics {count_index}'
 
 if __name__ == '__main__':
-    # start_http_server(9191)
+    start_http_server(8000)
     app.run(host="0.0.0.0", debug=True, port=8080)
